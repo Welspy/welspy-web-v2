df0749b15ca9059dcbf93c6dbe35ba10c59f2ed7
qtaghdi <135234546+qtaghdi@users.noreply.github.com>
2024-10-10 17:10:44 +0900

    "Feat : Route 추가"

--- src/components/routes/index.tsx
+++ src/components/routes/index.tsx
@@ -1,20 +1,22 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Home from "../../pages/home";
 import SignUp from "../../pages/auth/signup";
 import SignIn from "../../pages/auth/signin";
+import Product from "../../pages/product";
 
 const Router = () => {
-  return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<SignIn />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/sign-up" element={<SignUp />} />
-        </Routes>
-      </BrowserRouter>
-    </>
-  );
+    return (
+        <>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<SignIn/>}/>
+                    <Route path="/home" element={<Home/>}/>
+                    <Route path="/sign-up" element={<SignUp/>}/>
+                    <Route path="/product" element={<Product/>}/>
+                </Routes>
+            </BrowserRouter>
+        </>
+    );
 };
 
 export default Router;
