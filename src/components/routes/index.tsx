4bfb8f53c2bebf16cb120277c28ff16411c542b7
qtaghdi <135234546+qtaghdi@users.noreply.github.com>
2024-10-15 12:28:21 +0900

    "Fix : type 에러 해결"

--- src/components/routes/index.tsx
+++ src/components/routes/index.tsx
@@ -1,18 +1,20 @@
-import {BrowserRouter, Routes, Route} from "react-router-dom";
-import Home from "../../pages/home";
-import SignUp from "../../pages/auth/signup";
-import SignIn from "../../pages/auth/signin";
-import Product from "../../pages/product";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "src/pages/home/main";
+import SignUp from "src/pages/auth/signup";
+import SignIn from "src/pages/auth/signin";
+import Coalition from "src/pages/home/coalition";
+import Product from "src/pages/home/productList";
 
 const Router = () => {
     return (
         <>
             <BrowserRouter>
                 <Routes>
-                    <Route path="/" element={<SignIn/>}/>
-                    <Route path="/home" element={<Home/>}/>
-                    <Route path="/sign-up" element={<SignUp/>}/>
-                    <Route path="/product" element={<Product/>}/>
+                    <Route path="/" element={<SignIn />} />
+                    <Route path="/home" element={<Home />} />
+                    <Route path="/sign-up" element={<SignUp />} />
+                    <Route path="/coalition" element={<Coalition />} />
+                    <Route path="/product" element={<Product />} />
                 </Routes>
             </BrowserRouter>
         </>
--- src/modal/mychallengemodal/index.tsx
+++ src/modal/mychallengemodal/index.tsx
@@ -1,95 +1,95 @@
+import {AllChallengeProps} from "src/type/challenge.types";
 import * as S from "../style";
-import { AllChallengeProps } from "src/type/challenge.types";
+import {useEffect, useState} from "react";
 import UseProduct from "src/hooks/modal/useProduct";
-import { SetStateAction, useEffect, useState } from "react";
+import UseJoinChallenge from "src/hooks/modal/useJoinChallenge";
 
 interface Props {
-  RoomData: AllChallengeProps | undefined;
-  setModal: React.Dispatch<SetStateAction<boolean>>;
-  modal: boolean;
+    setModal: React.Dispatch<React.SetStateAction<boolean>>;
+    RoomData: AllChallengeProps | undefined;
+    modal: boolean;
 }
 
-const MyChallengeModal = ({ RoomData, setModal, modal }: Props) => {
-  const [productview, setProductView] = useState<boolean>(true);
-  const { product, Product } = UseProduct({ RoomData });
+const OtherChallengeModal = ({setModal, modal, RoomData}: Props) => {
+    const [productview, setProductView] = useState<boolean>(true);
+    const {product, Product} = UseProduct({RoomData});
+    const {JoinChallengeButton} = UseJoinChallenge({RoomData});
 
-  const ClickModal = () => {
-    setModal(!modal);
-  };
+    const ClickModal = () => {
+        setModal(!modal);
+    };
 
-  useEffect(() => {
-    Product();
-  }, []);
+    useEffect(() => {
+        Product();
+    }, []);
 
-  const Title = JSON.stringify(product?.name);
+    return (
+        <S.Wrapper>
+            {productview === true ? (
+                <S.PositionWrapper>
+                    <S.MainWrapper>
+                        <S.ModalDeleteButtonWrapper onClick={ClickModal}>X</S.ModalDeleteButtonWrapper>
+                        <S.MainContentWrapper>
+                            <S.ContentTitleWrapper>
+                                <S.TitleSpan>{RoomData?.title}</S.TitleSpan>
+                                <S.CategorySpanWrapper>
+                                    <S.CategorySpan>#{RoomData?.category}</S.CategorySpan>
+                                </S.CategorySpanWrapper>
+                            </S.ContentTitleWrapper>
+                            <S.GoalMoneyWrapper>
+                                <S.GoalMoneySpan>목표금액 : {RoomData?.goalMoney}</S.GoalMoneySpan>
+                                <S.MemberWrapper>
+                                    <S.MemberLimitSpan>멤버 제한 : {RoomData?.memberLimit}</S.MemberLimitSpan>
+                                    <S.CurrentMemberSpan>현재 멤버 : {RoomData?.currentMember}</S.CurrentMemberSpan>
+                                </S.MemberWrapper>
+                            </S.GoalMoneyWrapper>
+                            <S.ContenWrapper>
+                                <S.ChallengeImgWrapper>
+                                    <img src={RoomData?.imageUrl} style={{width: "100%", height: "100%"}} alt=""/>
+                                </S.ChallengeImgWrapper>
+                                <S.DescriptionWrapper>{RoomData?.description}</S.DescriptionWrapper>
+                            </S.ContenWrapper>
+                            <S.ChallengeJoinButton onClick={JoinChallengeButton}>챌린지 가입</S.ChallengeJoinButton>
+                        </S.MainContentWrapper>
+                    </S.MainWrapper>
+                    <button onClick={() => setProductView(!productview)}>제품 정보</button>
+                </S.PositionWrapper>
+            ) : (
+                <S.PositionWrapper>
+                    <S.MainWrapper>
+                        <S.ModalDeleteButtonWrapper onClick={ClickModal}>X</S.ModalDeleteButtonWrapper>
 
-
-  const price = JSON.stringify(product?.price);
-  const disPrice = JSON.stringify(product?.discountedPrice);
-  const discount = JSON.stringify(product?.discount);
-  const description = JSON.stringify(product?.description);
-  const imgUrl = JSON.stringify(product?.imageUrl);
-
-  return (
-    <S.Wrapper>
-      {productview === true ? (
-        <S.PositionWrapper>
-          <S.MainWrapper>
-            <S.ModalDeleteButtonWrapper onClick={ClickModal}>X</S.ModalDeleteButtonWrapper>
-            <S.MainContentWrapper>
-              <S.ContentTitleWrapper>
-                <S.TitleSpan>{RoomData?.title}</S.TitleSpan>
-                <S.CategorySpanWrapper>
-                  <S.CategorySpan>#{RoomData?.category}</S.CategorySpan>
-                </S.CategorySpanWrapper>
-              </S.ContentTitleWrapper>
-              <S.GoalMoneyWrapper>
-                <S.GoalMoneySpan>목표금액 : {RoomData?.goalMoney}</S.GoalMoneySpan>
-                <S.MemberWrapper>
-                  <S.MemberLimitSpan>멤버 제한 : {RoomData?.memberLimit}</S.MemberLimitSpan>
-                  <S.CurrentMemberSpan>현재 멤버 : {RoomData?.currentMember}</S.CurrentMemberSpan>
-                </S.MemberWrapper>
-              </S.GoalMoneyWrapper>
-              <S.ContenWrapper>
-                <S.ChallengeImgWrapper>
-                  <img src={RoomData?.imageUrl} alt="" />
-                </S.ChallengeImgWrapper>
-                <S.DescriptionWrapper>{RoomData?.description}</S.DescriptionWrapper>
-              </S.ContenWrapper>
-            </S.MainContentWrapper>
-          </S.MainWrapper>
-          <button onClick={() => setProductView(!productview)}>제품 정보</button>
-        </S.PositionWrapper>
-      ) : (
-        <S.PositionWrapper>
-          <S.MainWrapper>
-            <S.ModalDeleteButtonWrapper onClick={ClickModal}>X</S.ModalDeleteButtonWrapper>
-
-            <S.MainContentWrapper>
-              <S.ContentTitleWrapper>
-                <S.TitleSpan>제품 정보</S.TitleSpan>
-              </S.ContentTitleWrapper>
-              <S.ContentTitleWrapper>
-                <S.ProductNameSpan>{Title}</S.ProductNameSpan>
-              </S.ContentTitleWrapper>
-              <S.ChallengeImgWrapper>
-                <img src={imgUrl} alt="" />
-              </S.ChallengeImgWrapper>
-              <S.GoalMoneyWrapper>
-                <S.GoalMoneySpan style={{ color: "red" }}>가격 : {disPrice}</S.GoalMoneySpan>
-                <S.MemberWrapper style={{ flexDirection: "column" }}>
-                  <S.MemberLimitSpan>원가 : {price}</S.MemberLimitSpan>
-                  <S.CurrentMemberSpan style={{ color: " blue " }}>할인율 : {discount}</S.CurrentMemberSpan>
-                </S.MemberWrapper>
-              </S.GoalMoneyWrapper>
-              <S.DescriptionWrapper style={{ height: "40%", marginTop: 20 }}>{description}</S.DescriptionWrapper>
-            </S.MainContentWrapper>
-          </S.MainWrapper>
-          <button onClick={() => setProductView(!productview)}>챌린지 정보</button>
-        </S.PositionWrapper>
-      )}
-    </S.Wrapper>
-  );
+                        <S.MainContentWrapper>
+                            <S.ContentTitleWrapper>
+                                <S.TitleSpan>제품 정보</S.TitleSpan>
+                            </S.ContentTitleWrapper>
+                            <S.ContentTitleWrapper>
+                                <S.ProductNameSpan>{product?.name}</S.ProductNameSpan>
+                            </S.ContentTitleWrapper>
+                            <S.ChallengeImgWrapper>
+                                <img style={{width: "40%", height: "100%", objectFit: "cover"}} src={product?.imageUrl}
+                                     alt=""/>
+                            </S.ChallengeImgWrapper>
+                            <S.GoalMoneyWrapper>
+                                {/*<S.GoalMoneySpan style={{ color: "red" }}>가격 : {product?.discountedPrice}</S.GoalMoneySpan>*/}
+                                <S.MemberWrapper style={{flexDirection: "column"}}>
+                                    <S.MemberLimitSpan>원가 : {product?.price}</S.MemberLimitSpan>
+                                    <S.CurrentMemberSpan style={{color: " blue "}}>할인율
+                                        : {product?.discount}</S.CurrentMemberSpan>
+                                </S.MemberWrapper>
+                            </S.GoalMoneyWrapper>
+                            <S.DescriptionWrapper
+                                style={{height: "40%", marginTop: 20, whiteSpace: "pre-line", wordBreak: "break-word"}}
+                            >
+                                {product?.description}
+                            </S.DescriptionWrapper>
+                        </S.MainContentWrapper>
+                    </S.MainWrapper>
+                    <button onClick={() => setProductView(!productview)}>챌린지 정보</button>
+                </S.PositionWrapper>
+            )}
+        </S.Wrapper>
+    );
 };
 
-export default MyChallengeModal;
+export default OtherChallengeModal;
